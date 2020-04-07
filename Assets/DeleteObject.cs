6d092430bb129ca041f398808e2a143b3f6d1f13
koomorita <doraemon-.-love@ezweb.ne.jp>
2020-04-08 00:45:59 +0900

    Lesson6 課題提出
    Destroy関数を使い、アイテムを破棄する。

---
+++ Assets/DeleteObject.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeleteObject : MonoBehaviour {
+
+        
+    // Use this for initialization
+    void Start () {
+    
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnBecameInvisible()
+    {
+        //画面外の障害物を消去する。
+        GameObject.Destroy(this.gameObject);
+
+    }
+}
